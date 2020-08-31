bd8c3db13092afd800cd30bd555ed2e94632c25b
Grzegorz Sobczak <grzeg.sobczak@outlook.com>
2020-09-01 00:09:49 +0200

    Add header to float.h Fix stpCoordF_Add Add stpCoord and its operations

--- Marengo/coord.c
+++ Marengo/coord.c
@@ -12,10 +12,10 @@ stpCoordF_t stpCoordF_Zero()
 
 stpCoordF_t stpCoordF_Add(stpCoordF_t a, stpCoordF_t b)
 {
-  a.x = fsub(a.x, b.x);
-  a.y= fsub(a.y, b.y);
-  a.z = fsub(a.z, b.z);
-  a.e = fsub(a.e, b.e);
+  a.x = fadd(a.x, b.x);
+  a.y= fadd(a.y, b.y);
+  a.z = fadd(a.z, b.z);
+  a.e = fadd(a.e, b.e);
   return a;
 }
 
@@ -25,11 +25,42 @@ stpCoordF_t stpCoordFSub(stpCoordF_t a, stpCoordF_t b)
   ret.x=fsub(a.x,b.x);
   ret.y=fsub(a.y,b.y);
   ret.z=fsub(a.z,b.z);
-  ret.e=fsub(a.stpE,b.stpE);
+  ret.e=fsub(a.e,b.e);
   return ret;
 }
 
-float_t stpCoordF_Abs(stpCoordF_t a)
+uint32_t stpCoordF_Abs(stpCoordF_t a)
 {
   return 0; // TODO: Write coordinate length function
+}
+
+stpCoord_t stpCoord_Add(stpCoord_t a, stpCoord_t b)
+{
+  a.x+=b.x;
+  a.y+=b.y;
+  a.z+=b.z;
+  a.e+=b.e;
+  return a;
+}
+stpCoord_t stpCoord_Sub(stpCoord_t a, stpCoord_t b)
+{
+  a.x-=b.x;
+  a.y-=b.y;
+  a.z-=b.z;
+  a.e-=b.e;
+  return a;
+}
+uint32_t stpCoord_Abs(stpCoord_t a)
+{
+  uint64_t op = a.x*a.x+a.y*a.y+a.z*a.z;
+  return isqrt64(op);
+}
+stpCoord_t stpCoord_Zero(void)
+{
+  stpCoord_t a;
+  a.x=0;
+  a.y=0;
+  a.z=0;
+  a.e=0;
+  return a;
 }
--- Marengo/coord.h
+++ Marengo/coord.h
@@ -8,6 +8,8 @@
 #ifndef MARENGO_COORD_H_
 #define MARENGO_COORD_H_
 
+#include "float.h"
+
 typedef struct {
   float_t x;
   float_t y;
@@ -23,9 +25,21 @@ typedef struct {
   float_t e;
 } stpCoordF_t;
 
+typedef struct {
+  int32_t x;
+  int32_t y;
+  int32_t z;
+  int32_t e;
+} stpCoord_t;
+
 stpCoordF_t stpCoordF_Add(stpCoordF_t a, stpCoordF_t b);
 stpCoordF_t stpCoordF_Sub(stpCoordF_t a, stpCoordF_t b);
-int32_t stpCoordF_Abs(stpCoordF_t a);
+uint32_t stpCoordF_Abs(stpCoordF_t a);
 stpCoordF_t stpCoordF_Zero(void);
+
+stpCoord_t stpCoord_Add(stpCoord_t a, stpCoord_t b);
+stpCoord_t stpCoord_Sub(stpCoord_t a, stpCoord_t b);
+uint32_t stpCoord_Abs(stpCoord_t a);
+stpCoord_t stpCoord_Zero(void);
 
 #endif /* MARENGO_COORD_H_ */
